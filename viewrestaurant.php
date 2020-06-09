4a8799b9d4292cb32fedeb13a478cacbc89b1aef
wudiha <57778635+wudiha@users.noreply.github.com>
2020-06-09 12:45:06 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -6,17 +6,22 @@ $connectionOptions = array(
   "PWD" => "lingjiayee98!");                       
 //Establishes the connection       
 $conn = sqlsrv_connect($serverName, $connectionOptions);         
-if (!$conn) {            die("Error connection: ".sqlsrv_errors());        }        
+if (!$conn) {            
+  die("Error connection: ".sqlsrv_errors());        
+}        
 echo "DB SERVER:CONNECTED!" ;
+
 $tsql= "SELECT * FROM [dbo].[restaurant]";        
 $getResults= sqlsrv_query($conn, $tsql);                           
 if ($getResults == FALSE)  {             die(sqlsrv_errors());   }                          
  
+echo "<table border='1'>";
        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   {             
          echo "<tr>";             echo "<td>". $row['restaurant_id'] . "</td>";            
          echo "<td>". $row['restaurant_name'] ."</td>";            
          echo "<td>". $row['restaurant_address'] . "</td>";           
          echo "<td>". $row['restaurant_phone'] . "</td>";             
          echo "</tr>";         }        sqlsrv_free_stmt($getResults); 
+echo "</table>";
 ?> 
  
