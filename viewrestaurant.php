8b0d34105bfc021746db3c7cfb4490f194a77467
wudiha <57778635+wudiha@users.noreply.github.com>
2020-06-09 12:40:38 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,3 +1,21 @@
 <?php        
-  echo "DB Server: Connected!";
-?>
+$serverName = "testdb04195201.database.windows.net";        
+$connectionOptions = array(                             
+  "Database" => "testdb04195201",                            
+  "Uid" => "wudiha",                            
+  "PWD" => "lingjiayee98!");                       
+//Establishes the connection       
+$conn = sqlsrv_connect($serverName, $connectionOptions);         
+if (!$conn) {            die("Error connection: ".sqlsrv_errors());        }               
+$tsql= "SELECT * FROM [dbo].[restaurant]";        
+$getResults= sqlsrv_query($conn, $tsql);                           
+if ($getResults == FALSE)  {             die(sqlsrv_errors());   }                          
+ 
+       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   {             
+         echo "<tr>";             echo "<td>". $row['restaurant_id'] . "</td>";            
+         echo "<td>". $row['restaurant_name'] ."</td>";            
+         echo "<td>". $row['restaurant_address'] . "</td>";           
+         echo "<td>". $row['restaurant_phone'] . "</td>";             
+         echo "</tr>";         }        sqlsrv_free_stmt($getResults); 
+?> 
+ 
