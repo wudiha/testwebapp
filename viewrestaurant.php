7ea9f08d2e844ff2d906140b671923eacdd3d3e1
wudiha <57778635+wudiha@users.noreply.github.com>
2020-06-09 12:47:22 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,9 +1,9 @@
 <?php        
-$serverName = "testdb04195201.database.windows.net";        
-$connectionOptions = array(                             
-  "Database" => "testdb04195201",                            
-  "Uid" => "wudiha",                            
-  "PWD" => "lingjiayee98!");                       
+  $serverName = "testdb04195201.database.windows.net";        
+  $connectionOptions = array(                             
+    "Database" => "testdb04195201",                            
+    "Uid" => "wudiha",                            
+    "PWD" => "lingjiayee98!");                       
 //Establishes the connection       
 $conn = sqlsrv_connect($serverName, $connectionOptions);         
 if (!$conn) {            
@@ -13,11 +13,14 @@ echo "DB SERVER:CONNECTED.." ;
 
 $tsql= "SELECT * FROM [dbo].[restaurant]";        
 $getResults= sqlsrv_query($conn, $tsql);                           
-if ($getResults == FALSE)  {             die(sqlsrv_errors());   }                          
+if ($getResults == FALSE)  {             
+  die(sqlsrv_errors());   
+}                          
  
 echo "<table border='1'>";
        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   {             
-         echo "<tr>";             echo "<td>". $row['restaurant_id'] . "</td>";            
+         echo "<tr>";             
+         echo "<td>". $row['restaurant_id'] . "</td>";            
          echo "<td>". $row['restaurant_name'] ."</td>";            
          echo "<td>". $row['restaurant_address'] . "</td>";           
          echo "<td>". $row['restaurant_phone'] . "</td>";             
