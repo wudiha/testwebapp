9504cd42597e2f29383869761a78bd3db7c4d82b
wudiha <57778635+wudiha@users.noreply.github.com>
2020-06-09 12:29:52 +0800

    Create viewrestaurant.php

---
+++ viewrestaurant.php
@@ -0,0 +1,14 @@
+<?php        
+  $serverName = "testdb04195201.database.windows.net";         
+  $connectionOptions = array(                             
+    "Database" => "testdb04195201",                             
+    "Uid" => "wudiha",                             
+    "PWD" => "lingjiayee98!");                          
+  //Establishes the connection        
+  $conn = sqlsrv_connect($serverName, $connectionOptions);         
+  if (!$conn) 
+  {            
+  die("Error connection: ".sqlsrv_errors());       
+  }
+  echo "DB Server: Connected!";
+}   
